450604e019f77716b390d717a0b445dc323cb833
mpollachek <mpollachek81@gmail.com>
2022-10-30 01:10:18 -0400

    set headers

--- server.js
+++ server.js
@@ -13,12 +13,9 @@ app.use(cors({
   credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "allworkersunion.com"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
