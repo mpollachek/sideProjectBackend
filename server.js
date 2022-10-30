6e480605a7e7f78ed87a66c21e20649c801c1c75
mpollachek <mpollachek81@gmail.com>
2022-10-30 01:01:20 -0400

    set headers

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const newsletterEmailsModel = require('./models/NewsletterEmails');
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
