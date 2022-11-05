2c6f8d55ffcedca3295bb8b3a6ecc0e37bb0d5b6
mpollachek <mpollachek81@gmail.com>
2022-11-04 22:56:10 -0400

    Cors preflight issues

--- server.js
+++ server.js
@@ -18,9 +18,12 @@ app.use(function(req, res, next) {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+res.setHeader('Access-Control-Allow-Credentials', true);
     // res.header('Access-Control-Allow-HEaders', 'Content-Type, Authorization');
   }
-  return next();
+  next();
 });
 
 
