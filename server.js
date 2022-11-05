b4dea7ceeff2efd28f342c5a021b7b73e7e2ddbc
mpollachek <mpollachek81@gmail.com>
2022-11-04 23:41:16 -0400

    Cors preflight issues

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors({
 //   next();
 // }});
 
-app.all('/', function(req, res, next) {
+app.all('/GetInvolved', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next()
