209b6d4f8316b837a2226d0e31d3ea4f9e4796c2
mpollachek <mpollachek81@gmail.com>
2022-11-06 13:08:07 -0500

    CORS issues

--- routes/getInvolvedRouter.js
+++ routes/getInvolvedRouter.js
@@ -1,29 +1,42 @@
 const express = require('express');
 const GetInvolvedEmailsModel = require('../models/NewsletterEmails');
+const StrikeEmailsModel = require('../models/StrikeEmails');
+const cors = require('cors');
 
 const getInvolvedRouter = express.Router();
 
+getInvolvedRouter.use(cors())
 
-getInvolvedRouter.route('/GetInvolvedEmails')
+
+getInvolvedRouter.route("/api/getEmails")
 .get((req, res, next) => {
-  GetInvolvedEmailsModel.find()
-  .then(email => {
-    console.log('email saved: ', email);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(email);
-  })
-  .catch(err => next(err));
+  res.set('Access-Control-Allow-Origin', '*');
+  StrikeEmailsModel.countDocuments({}, (err, result) => {
+    if (err) {
+      res.json("Error: " + err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+getInvolvedRouter.route("/api/addEmail")
+.post( async (req, res, next) => {
+  const r = req.body.values
+  console.log("full req: " + JSON.stringify(r))
+  if (r.strike) {
+    const strikeEmail= new StrikeEmailsModel(r);
+    await strikeEmail.save();
+  } 
+
+  if (r.newsletter) {
+    const newsletterEmail= new newsletterEmailsModel(r);
+    await newsletterEmail.save();
+  } 
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json(r);
 })
 
-getInvolvedRouter.route('/GetInvolvedEmails')
-.post((req, res, next) => {
-  GetInvolvedEmailsModel.create(req.body)
-  .then(email => {
-    console.log('email saved: ', email);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json(email);
-  })
-  .catch(err => next(err));
-})
+module.exports = getInvolvedRouter;
--- server.js
+++ server.js
@@ -8,11 +8,21 @@ const newsletterEmailsModel = require('./models/NewsletterEmails');
 
 const cors = require("cors");
 
+// app.use(cors())
+
 app.use(express.json());
 app.use(cors({
   origin: ['http://localhost:3000', 'https://www.allworkersunion.com'],
   credentials: true
 }));
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.allworkersunion.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+  });
 
 //app.use('/api/*', createProxyMiddleware({target:'*', changeOrigin: true }));
 
@@ -27,16 +37,16 @@ app.use(cors({
 //   next();
 // }});
 
-app.all('/GetInvolved', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
-});
+// app.all('/GetInvolved', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next()
+// });
 
 
-//const getInvolvedRouter = require('./routes/getInvolvedRouter')
-//const uri = `mongodb+srv://mpollachek81:${mongoPassword}@awu0.${mongoString}.mongodb.net/allWorkersUnionDB?retryWrites=true&w=majority`;
-const port = process.env.PORT || 3000;
+// const getInvolvedRouter = require('./routes/getInvolvedRouter');
+// const uri = `mongodb+srv://mpollachek81:${mongoPassword}@awu0.${mongoString}.mongodb.net/allWorkersUnionDB?retryWrites=true&w=majority`;
+const port = process.env.PORT || 3001;
 
 const uri = process.env.MONGODB_URI;
 
@@ -46,10 +56,16 @@ mongoose.connect(uri, {
 })
 .then(() => {
   app.listen(port);
-  console.log("Server is Running - booyah");
+  console.log("Server is Running - booyah - " + port);
 });
 
-//app.use('/GetInvolvedEmails', getInvolvedRouter);
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use('/GetInvolvedEmails', getInvolvedRouter);
+
+
 
 app.get("/api/getEmails", (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
