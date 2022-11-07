2668833a9aa011d53ac5e2a19017402eab633065
mpollachek <mpollachek81@gmail.com>
2022-11-06 23:51:14 -0500

    stable - only allows site to make requests

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 const StrikeEmailsModel = require('./models/StrikeEmails');
 const newsletterEmailsModel = require('./models/NewsletterEmails');
 // const { mongoPassword, mongoString } = require('./config');
-//const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const cors = require("./routes/cors");
 
@@ -25,26 +24,6 @@ app.use(function (req, res, next) {
   next();
   });
 
-//app.use('/api/*', createProxyMiddleware({target:'*', changeOrigin: true }));
-
-// app.use(function(req, res, next) {
-//   const allowedOrigins = ['http://localhost:3000', 'https://www.allworkersunion.com'];
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//     res.setHeader('Access-Control-Allow-Origin', origin);
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// }});
-
-// app.all('/GetInvolved', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   next()
-// });
-
-
 // const getInvolvedRouter = require('./routes/getInvolvedRouter');
 // const uri = `mongodb+srv://mpollachek81:${mongoPassword}@awu0.${mongoString}.mongodb.net/allWorkersUnionDB?retryWrites=true&w=majority`;
 const port = process.env.PORT || 3001;
@@ -60,13 +39,7 @@ mongoose.connect(uri, {
   console.log("Server is Running - booyah - " + port);
 });
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
 // app.use('/GetInvolvedEmails', getInvolvedRouter);
-
-
 
 app.route("/api/getEmails")
 .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
@@ -97,22 +70,3 @@ app.route("/api/addEmail")
   res.statusCode = 200;
   res.json(r);
 });
-
-// app.post("/api/addEmail", async (req, res) => {
-//   const r = req.body.values
-//   console.log("full req: " + JSON.stringify(r))
-//   if (r.strike) {
-//     const strikeEmail= new StrikeEmailsModel(r);
-//     await strikeEmail.save();
-//   } 
-
-//   if (r.newsletter) {
-//     const newsletterEmail= new newsletterEmailsModel(r);
-//     await newsletterEmail.save();
-//   } 
-
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'application/json');
-//   res.json(r);
-// })
-
