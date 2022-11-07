95f05e711c96555a9efc31ea2dd47f8eb37bc286
mpollachek <mpollachek81@gmail.com>
2022-11-06 23:32:31 -0500

    CORS issues

--- routes/cors.js
+++ routes/cors.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const whitelist = ['http://localhost:3000', 'https://www.allworkersunion.com'];
 const corsOptionsDelegate = async (req, callback) => {
     let corsOptions = {
+      credentals: true,
       allowedHeaders: 'Content-Type, Authorization',
     }
     console.log(req.header('Origin'));
